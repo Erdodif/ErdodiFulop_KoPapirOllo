d04fef39b60b26c09b9a28aadeedf73f5e757d83
Erdodif <erdodif@gmail.com>
2021-10-20 19:47:38 +0200

    Játék lezárása, újrajátszása és a fenti, véglelenül idegesítő elrendezés kijavítása (weightSum és elemenként weight)

--- app/src/main/java/hu/petrik/kopapirollo/MainActivity.kt
+++ app/src/main/java/hu/petrik/kopapirollo/MainActivity.kt
@@ -5,22 +5,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import hu.petrik.kopapirollo.databinding.ActivityMainBinding
 import java.util.*
+import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
     private lateinit var bind: ActivityMainBinding
 
-    private var gepElet = 3;
-    private var felhasznaloElet = 3;
-    private var gep = 0;
+    private var dontetlenek = 0
+    private var gepElet = 3
+    private var felhasznaloElet = 3
+    private var gep = 0
     private var random: Random = Random()
 
-    private fun eletAllit(elet: Int,jatekos : Boolean) {
+    private fun eletAllit(elet: Int, jatekos: Boolean) {
         val elet3 = if (elet < 3) R.drawable.heart1 else R.drawable.heart2
         val elet2 = if (elet < 2) R.drawable.heart1 else R.drawable.heart2
         val elet1 = if (elet < 1) R.drawable.heart1 else R.drawable.heart2
-        if(jatekos){
+        if (jatekos) {
             bind.imageFelhasznalo1.setImageResource(elet1)
             bind.imageFelhasznalo2.setImageResource(elet2)
             bind.imageFelhasznalo3.setImageResource(elet3)
@@ -42,14 +45,44 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun ujra(gyozelem: Boolean) {
+        val fejlec = if (gyozelem) "Győzelem" else "Vereség"
+        var dialogus = AlertDialog.Builder(this)
+        dialogus.setTitle(fejlec)
+        dialogus.setMessage("Szeretne Újból játszani?")
+        dialogus.setPositiveButton("Igen") { _, _ ->
+            dontetlenek = 0
+            gepElet = 3
+            felhasznaloElet = 3
+            gep = 0
+            eletAllit(3, true)
+            eletAllit(3, false)
+        }
+        dialogus.setNegativeButton("Nem") { _, _ ->
+            exitProcess(0)
+        }
+        dialogus.setOnDismissListener {
+            dontetlenek = 0
+            gepElet = 3
+            felhasznaloElet = 3
+            gep = 0
+            eletAllit(3, true)
+            eletAllit(3, false)
+        }
+        dialogus.show()
+    }
+
     private fun jatszma(mi: Int) {
         kepModosit(mi, bind.imageViewFelhasznalo)
         gep = random.nextInt(3)
         kepModosit(gep, bind.imageViewGep)
-        if (mi == gep){
+        val eredmeny = mi - gep
+        if (eredmeny == 0) {
+            dontetlenek++
+            val kiad = "${getString(R.string.dontetlenek)} ${dontetlenek}"
+            bind.textViewDontetlenek.setText(kiad)
             Toast.makeText(this, "Döntetlen", Toast.LENGTH_SHORT).show()
-        }
-        else if(mi - gep == 1 || mi - gep == -2){
+        } else if (eredmeny == 1 || eredmeny == -2) {
             //játékos győzelem az alábbiak szerint:
             //p1 - k0 = 1   Győzelem
             //o2 - k0 = 2
@@ -57,11 +90,30 @@ class MainActivity : AppCompatActivity() {
             //p1 - o2 = -1
             //k0 - p1 = -1
             //o2 - p1 = 1   Győzelem
-            Toast.makeText(this, "Gőzelem", Toast.LENGTH_SHORT).show()
-        }
-        else{
+            gepElet--
+            eletAllit(gepElet, false)
+            if (gepElet == 0) {
+                Toast.makeText(
+                    this, "Győzelem a gépek felett",
+                    Toast.LENGTH_SHORT
+                ).show()
+                ujra(true)
+            } else {
+                Toast.makeText(this, "Gőzelem", Toast.LENGTH_SHORT).show()
+            }
+        } else {
             //gép győzelem
-            Toast.makeText(this, "Vereség", Toast.LENGTH_SHORT).show()
+            felhasznaloElet--
+            eletAllit(felhasznaloElet, true)
+            if (felhasznaloElet == 0) {
+                Toast.makeText(
+                    this, "Vereség a gép ellen",
+                    Toast.LENGTH_SHORT
+                ).show()
+                ujra(false)
+            } else {
+                Toast.makeText(this, "Vereség", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
